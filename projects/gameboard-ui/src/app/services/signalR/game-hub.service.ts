c41a909ad80c7e1eabf03b111e0276db38306760
Ben Stein <bsstein@sei.cmu.edu>
2023-03-27 16:43:54 -0400

    Frontend stuff for sync start - new hub service which manages game-level events (on the same signalR hub as the one previously used).

---
+++ projects/gameboard-ui/src/app/services/signalR/game-hub.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { SyncStartState } from '../../game/game.models';
+import { SignalrService } from './signalr.service';
+
+export interface GameHubEvent {
+    gameId: string;
+    eventType: GameHubEventType;
+    gameEventData: any;
+}
+
+enum GameHubEventType {
+    PlayerReadyStateChanged
+}
+
+@Injectable({ providedIn: 'root' })
+export class GameHubService implements OnDestroy {
+    private gameHubEventsSub?: Subscription;
+    private syncStartStateChanged$ = new Subject<SyncStartState>();
+
+    constructor(private signalr: SignalrService) {
+        this.signalr.gameHubEvents$.subscribe(ev => {
+            switch (ev.data.eventType) {
+                case GameHubEventType.PlayerReadyStateChanged:
+                    this.handlePlayerReadyStateChanged(ev.data);
+                    return;
+                default:
+                    return;
+            }
+        });
+    }
+
+    private handlePlayerReadyStateChanged(ev: GameHubEvent) {
+        this.syncStartStateChanged$.next(ev.gameEventData as SyncStartState);
+    }
+
+    ngOnDestroy(): void {
+        throw new Error('Method not implemented.');
+    }
+}
---
+++ projects/gameboard-ui/src/app/services/signalR/signalr.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { HttpTransportType, HubConnection, HubConnectionBuilder, HubConnectionState, IHttpConnectionOptions, LogLevel } from '@microsoft/signalr';
+import { map } from 'rxjs/operators';
+import { UserService } from '../../api/user.service';
+import { ConfigService } from '../../utility/config.service';
+import { LogService } from '../log.service';
+import { GameHubEvent } from './game-hub.service';
+
+export interface SignalRHubEvent<T> {
+  channelId: string;
+  data: T;
+}
+
+export interface SignalRHubState {
+  connectionState: HubConnectionState;
+}
+
+interface SignalRHubStateUpdate {
+  connectionState?: HubConnectionState;
+}
+
+@Injectable({ providedIn: 'root' })
+export class SignalrService {
+  private signalRConnection: HubConnection;
+  private _gameHubEvents$ = new Subject<SignalRHubEvent<GameHubEvent>>;
+  private _state$ = new BehaviorSubject<SignalRHubState>({ connectionState: HubConnectionState.Disconnected });
+
+  public gameHubEvents$ = this._gameHubEvents$.asObservable();
+  public state$ = this._state$.asObservable();
+
+  constructor(
+    config: ConfigService,
+    private logger: LogService,
+    private userService: UserService) {
+    this.signalRConnection = this.getConnection(`${config.apphost}hub`);
+  }
+
+  private getConnection(url: string): HubConnection {
+    const connection = new HubConnectionBuilder()
+      .withUrl(url, {
+        accessTokenFactory: () => this.userService.ticket().pipe(map(result => result.ticket)).toPromise(),
+        transport: HttpTransportType.WebSockets,
+        skipNegotiation: true,
+      } as IHttpConnectionOptions)
+      .withAutomaticReconnect([1000, 2000, 3000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000])
+      .configureLogging(LogLevel.Information)
+      .build();
+
+    connection.onclose(this.handleClose);
+    connection.onreconnected(cid => this.handleConnected);
+
+    // federate specific event types to their respective services
+    connection.on('gameHubEvent', (ev: SignalRHubEvent<GameHubEvent>) => this._gameHubEvents$.next(ev));
+
+    return connection;
+  }
+
+  private handleConnected(channelId: string) {
+    this.logger.logInfo(`SignalR Service | Connected -> ${channelId}`);
+  }
+
+  private handleClose(err?: Error) {
+    if (err) {
+      this.logger.logError(`SignalR Service Error: ${err}`);
+    }
+
+    this.updateConnectionState();
+  }
+
+  private updateConnectionState() {
+    this.updateState({
+      connectionState: this.signalRConnection.state
+    });
+  }
+
+  private updateState(update: SignalRHubStateUpdate) {
+    const currentState = this._state$.getValue();
+    const finalState: SignalRHubState = {
+      connectionState: update.connectionState || currentState.connectionState
+    };
+
+    this._state$.next(finalState);
+  }
+}
