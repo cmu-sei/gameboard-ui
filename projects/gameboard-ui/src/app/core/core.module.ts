f56f31705d70bc6f2c376dc3c2f38c4f680a3425
Ben Stein <bsstein@sei.cmu.edu>
2023-06-14 12:20:27 -0400

    Added a component that allows inline rendering of links while preserving user-entered text. Resolves #204.

--- projects/gameboard-ui/src/app/core/core.module.ts
+++ projects/gameboard-ui/src/app/core/core.module.ts
@@ -5,8 +5,9 @@ import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { GbProgressBarComponent } from './components/progress-bar/progress-bar.component';
-import { LinkifyHtmlPipe } from './pipes/linkify-html.pipe';
 import { LongContentHiderComponent } from './components/long-content-hider/long-content-hider.component';
+import { LinkRendererPipe } from './pipes/link-renderer.pipe';
+import { RenderLinksInTextComponent } from './components/render-links-in-text/render-links-in-text.component';
 import { ModalConfirmComponent } from './components/modal/modal-confirm.component';
 import { ModalConfirmDirective } from './directives/modal-confirm.directive';
 import { NumbersToPercentage } from './pipes/numbers-to-percentage.pipe';
@@ -14,16 +15,14 @@ import { PlayerAvatarComponent } from './components/player-avatar/player-avatar.
 import { PlayerAvatarListComponent } from './components/player-avatar-list/player-avatar-list.component';
 import { PlayerStatusComponent } from './components/player-status/player-status.component';
 import { RelativeUrlsPipe } from './pipes/relative-urls.pipe';
-import { SanitizeHtmlPipe } from './pipes/sanitize-html.pipe';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { ToggleSwitchComponent } from './components/toggle-switch/toggle-switch.component';
 import { UrlRewritePipe } from './pipes/url-rewrite.pipe';
 import { YamlBlockComponent } from './components/yaml-block/yaml-block.component';
 import { YamlPipe } from './pipes/yaml.pipe';
-import { CustomInputComponent } from './components/custom-input/custom-input.component';
 
 const PUBLIC_DECLARATIONS = [
-  LinkifyHtmlPipe,
+  LinkRendererPipe,
   LongContentHiderComponent,
   ModalConfirmComponent,
   ModalConfirmDirective,
@@ -33,7 +32,7 @@ const PUBLIC_DECLARATIONS = [
   GbProgressBarComponent,
   NumbersToPercentage,
   RelativeUrlsPipe,
-  SanitizeHtmlPipe,
+  RenderLinksInTextComponent,
   SpinnerComponent,
   UrlRewritePipe,
   ToggleSwitchComponent,
