b17da9985149736fe44defa13e8f6aec6d720191
Ben Stein <bsstein@sei.cmu.edu>
2023-10-30 12:58:25 -0400

    Fixed an issue that caused the practice area to offer a link to the certificate a user receives upon successful practice completion (even if the certificate wasn't configured in admin.

--- projects/gameboard-ui/src/app/prac/components/practice-challenge-solved-modal/practice-challenge-solved-modal.component.ts
+++ projects/gameboard-ui/src/app/prac/components/practice-challenge-solved-modal/practice-challenge-solved-modal.component.ts
@@ -1,8 +1,10 @@
 import { LocalActiveChallenge } from '@/api/challenges.models';
 import { PlayerMode } from '@/api/player-models';
+import { PracticeService } from '@/services/practice.service';
 import { RouterService } from '@/services/router.service';
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { firstValueFrom } from 'rxjs';
 
 export interface PracticeChallengeSolvedModalContext {
   challenge: LocalActiveChallenge;
@@ -16,16 +18,20 @@ export interface PracticeChallengeSolvedModalContext {
 export class PracticeChallengeSolvedModalComponent implements OnInit {
   protected context?: PracticeChallengeSolvedModalContext;
   protected certificateUrl?: string;
+  protected isCertificateConfigured = false;
 
   constructor(
+    private practiceService: PracticeService,
     private routerService: RouterService,
     private modalRef: BsModalRef) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     if (!this.context) {
       throw new Error("Can't resolve the context for the PracticeChallengeSolvedModalComponent.");
     }
 
+    const practiceSettings = await firstValueFrom(this.practiceService.getSettings());
+    this.isCertificateConfigured = !!practiceSettings.certificateHtmlTemplate;
     this.certificateUrl = this.routerService.getCertificatePrintableUrl(PlayerMode.practice, this.context.challenge.spec.id);
   }
 
