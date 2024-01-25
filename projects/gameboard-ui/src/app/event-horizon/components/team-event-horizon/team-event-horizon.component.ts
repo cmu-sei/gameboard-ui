b64af058b2a5b0e3429e6d50c8d804caf6493722
Ben Stein <bsstein@sei.cmu.edu>
2024-01-25 11:56:42 -0500

    Handle missing event horizon data.

--- projects/gameboard-ui/src/app/event-horizon/components/team-event-horizon/team-event-horizon.component.ts
+++ projects/gameboard-ui/src/app/event-horizon/components/team-event-horizon/team-event-horizon.component.ts
@@ -18,9 +18,8 @@ export class TeamEventHorizonComponent implements OnInit, AfterViewInit, OnDestr
 
   protected selectedEventTypes: EventHorizonEventType[] = [];
   protected allEventTypes: EventHorizonEventType[] = [];
+  protected timelineViewModel: TeamEventHorizonViewModel | null = null;
   private timeline?: Timeline;
-
-  private timelineViewModel?: TeamEventHorizonViewModel;
 
   constructor(
     private eventHorizonService: EventHorizonService,
@@ -38,12 +37,17 @@ export class TeamEventHorizonComponent implements OnInit, AfterViewInit, OnDestr
   async ngAfterViewInit(): Promise<void> {
     if (!this.teamId)
       throw new Error("Can't use the team event horizon component without a team Id.");
-    if (!this.timelineContainer)
-      throw new Error("Couldn't find the timeline container.");
 
     // Create a Timeline
     // (apparently this just does the thing, which is weird, but whatever)
     this.timelineViewModel = await this.eventHorizonService.getTeamEventHorizon(this.teamId);
+
+    if (!this.timelineViewModel)
+      return;
+
+    if (!this.timelineContainer)
+      throw new Error("Couldn't find the timeline container.");
+
     const timelineViewOptions = this.eventHorizonRenderingService.getViewOptions(this.timelineViewModel);
     const visibleDataItems = this.buildTimelineDataSet(this.timelineViewModel);
 
@@ -92,6 +96,9 @@ export class TeamEventHorizonComponent implements OnInit, AfterViewInit, OnDestr
   }
 
   protected async handleEventTypeToggled(eventType: EventHorizonEventType) {
+    if (!this.timelineViewModel)
+      return;
+
     if (this.selectedEventTypes.some(t => t === eventType)) {
       this.selectedEventTypes = [...this.selectedEventTypes.filter(t => t !== eventType)];
     }
