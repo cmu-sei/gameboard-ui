6148706e677b5f7303e8c9d2f51b020d4557188b
Ben Stein <bsstein@sei.cmu.edu>
2024-03-20 13:35:30 -0400

    Migrate #400 fix from test

--- projects/gameboard-ui/src/app/core/directives/refresh-iframe-on-reconnect.directive.ts
+++ projects/gameboard-ui/src/app/core/directives/refresh-iframe-on-reconnect.directive.ts
@@ -1,25 +1,31 @@
 import { ApiStatusService } from '@/api/api-status.service';
 import { UnsubscriberService } from '@/services/unsubscriber.service';
-import { Directive, ElementRef } from '@angular/core';
+import { ConfigService } from '@/utility/config.service';
+import { ToastService } from '@/utility/services/toast.service';
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 
 @Directive({
   providers: [UnsubscriberService],
   selector: '[appRefreshIframeOnReconnect]'
 })
-export class RefreshIframeOnReconnectDirective {
+export class RefreshIframeOnReconnectDirective implements AfterViewInit {
   private iframe?: HTMLIFrameElement;
 
   constructor(
-    apiStatusService: ApiStatusService,
-    ref: ElementRef,
-    unsub: UnsubscriberService) {
-    if (!ref?.nativeElement?.contentWindow?.location?.reload())
+    private apiStatusService: ApiStatusService,
+    private config: ConfigService,
+    private ref: ElementRef,
+    private toastService: ToastService,
+    private unsub: UnsubscriberService) { }
+
+  ngAfterViewInit(): void {
+    if (!this.ref?.nativeElement?.contentWindow?.location?.reload())
       throw new Error("Can't use appRefreshIframeOnReconnect with a non-refreshable IFrame.");
 
-    this.iframe = ref.nativeElement;
-
-    unsub.add(apiStatusService.status$.subscribe(status => {
+    this.iframe = this.ref.nativeElement;
+    this.unsub.add(this.apiStatusService.status$.subscribe(status => {
       if (status === "up") {
+        this.toastService.showMessage(`${this.config.appName} experienced an interruption in service. We're reloading your game to ensure you get the best experience possible.`)
         this.iframe?.contentWindow?.location?.reload();
       }
     }));
