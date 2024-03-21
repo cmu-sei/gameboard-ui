be74a6c1ea0cd90cf458b68fe2a512c3850da2a3
Ben Stein <bsstein@sei.cmu.edu>
2024-03-21 14:33:32 -0400

    Fix stray refresh bug for external games

--- projects/gameboard-ui/src/app/core/directives/refresh-iframe-on-reconnect.directive.ts
+++ projects/gameboard-ui/src/app/core/directives/refresh-iframe-on-reconnect.directive.ts
@@ -10,6 +10,7 @@ import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 })
 export class RefreshIframeOnReconnectDirective implements AfterViewInit {
   private iframe?: HTMLIFrameElement;
+  private hasInitialApiState = false;
 
   constructor(
     private apiStatusService: ApiStatusService,
@@ -24,6 +25,12 @@ export class RefreshIframeOnReconnectDirective implements AfterViewInit {
 
     this.iframe = this.ref.nativeElement;
     this.unsub.add(this.apiStatusService.status$.subscribe(status => {
+      // don't react for the initial value
+      if (!this.hasInitialApiState) {
+        this.hasInitialApiState = true;
+        return;
+      }
+
       if (status === "up") {
         this.toastService.showMessage(`${this.config.appName} experienced an interruption in service. We're reloading your game to ensure you get the best experience possible.`);
         this.iframe?.contentWindow?.location?.reload();
