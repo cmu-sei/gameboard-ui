a6bb473d5cf25f6adefa6e2f0e94a213e2caffdb
Ben Stein <115497763+sei-bstein@users.noreply.github.com>
2024-10-16 15:05:43 -0400

    v3.23.3 (#198)
    * Fix tracking of active prac challenge
    
    * Minor cleanup and improved console logging
    
    * Bug fixes and challenge report tags filter.
    
    * Fix event horizon, game center teams, enrollment report stat summary, progress widget
    
    * Fix issue with past games not showing ongoing ones
    
    * Report improvements, game card rendering
    
    * Fix tag rendering in practice reports

--- projects/gameboard-ui/src/app/core/components/game-card-image/game-card-image.component.ts
+++ projects/gameboard-ui/src/app/core/components/game-card-image/game-card-image.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-card-image',
+  styles: [
+    ":host { aspect-ratio: 1/1.44; display: block; }",
+    ".game-card-container { aspect-ratio: 1/1.44;  background-size: cover; }"
+  ],
   templateUrl: './game-card-image.component.html'
 })
 export class GameCardImageComponent {
   @Input() game?: { id: string; name: string; logo?: string };
-  @Input() width: string = "100%";
 }
