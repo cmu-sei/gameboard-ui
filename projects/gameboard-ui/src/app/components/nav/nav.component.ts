7463bbbe460711a3f64c80044aae00ef108ce99d
Ben Stein <bsstein@sei.cmu.edu>
2024-10-30 16:56:33 -0400

    Improvements to OIDC experience and support for auto log in/out.

--- projects/gameboard-ui/src/app/components/nav/nav.component.ts
+++ projects/gameboard-ui/src/app/components/nav/nav.component.ts
@@ -4,10 +4,8 @@ import { ApiUser } from '@/api/user-models';
 import { UserService as LocalUser } from '@/utility/user.service';
 import { PracticeService } from '@/services/practice.service';
 import { UnsubscriberService } from '@/services/unsubscriber.service';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { fa } from '@/services/font-awesome.service';
-import { AuthService } from '@/utility/auth.service';
-import { RouterService } from '@/services/router.service';
 
 @Component({
   selector: 'app-nav',
@@ -16,9 +14,6 @@ import { RouterService } from '@/services/router.service';
   providers: [UnsubscriberService]
 })
 export class AppNavComponent implements OnInit {
-  private authService = inject(AuthService);
-  private routerService = inject(RouterService);
-
   user$!: Observable<ApiUser | null>;
   toc$!: Observable<TocFile[]>;
   customBackground = "";
@@ -27,7 +22,6 @@ export class AppNavComponent implements OnInit {
   protected fa = fa;
   protected isCollapsed = false;
   protected isPracticeModeEnabled = false;
-  protected profileUrl = "";
 
   constructor(
     private localUser: LocalUser,
@@ -39,7 +33,6 @@ export class AppNavComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     this.user$ = this.localUser.user$;
     this.toc$ = this.toc.toc$;
-    this.profileUrl = this.routerService.getProfileUrl();
 
     this.isPracticeModeEnabled = await this.practiceService.isEnabled();
     this.unsub.add(this.practiceService.isEnabled$.subscribe(isEnabled => this.updatePracticeModeEnabled(isEnabled)));
@@ -47,13 +40,5 @@ export class AppNavComponent implements OnInit {
 
   private updatePracticeModeEnabled(isEnabled: boolean) {
     this.isPracticeModeEnabled = isEnabled;
-  }
-
-  protected async handleLogIn() {
-    await this.authService.login();
-  }
-
-  protected handleLogOut() {
-    this.authService.logout();
   }
 }
