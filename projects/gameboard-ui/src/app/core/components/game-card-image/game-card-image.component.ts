2861c4495018252e5c3131990f67f9eabb07c721
Ben Stein <115497763+sei-bstein@users.noreply.github.com>
2025-01-24 10:54:34 -0500

    Tweak to game card image css (#225)

--- projects/gameboard-ui/src/app/core/components/game-card-image/game-card-image.component.ts
+++ projects/gameboard-ui/src/app/core/components/game-card-image/game-card-image.component.ts
@@ -2,10 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-card-image',
-  styles: [
-    ":host { aspect-ratio: 1/1.44; display: block; }",
-    ".game-card-container { aspect-ratio: 1/1.44; background: #303030 center center/contain no-repeat; background-position: contain }"
-  ],
+  styleUrls: ["./game-card-image.component.scss"],
   templateUrl: './game-card-image.component.html'
 })
 export class GameCardImageComponent {
