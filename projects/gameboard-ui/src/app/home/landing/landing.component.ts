d6b667917416a73778ced208b3038f83a97720c2
Ben Stein <bsstein@sei.cmu.edu>
2025-02-17 12:50:23 -0500

    the landing page now calls-to-action for the practice area if there aren't any upcoming games.

--- projects/gameboard-ui/src/app/home/landing/landing.component.ts
+++ projects/gameboard-ui/src/app/home/landing/landing.component.ts
@@ -1,8 +1,8 @@
 // Copyright 2021 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, UrlTree } from '@angular/router';
 import { faGamepad, faPlusSquare, faSearch, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { debounceTime, switchMap, tap } from 'rxjs/operators';
@@ -11,13 +11,16 @@ import { Game, GameGroup } from '../../api/game-models';
 import { GameService } from '../../api/game.service';
 import { RouterService } from '@/services/router.service';
 import { fa } from '@/services/font-awesome.service';
+import { PracticeService } from '@/services/practice.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+  private practiceService = inject(PracticeService);
+
   refresh$ = new BehaviorSubject<any>(true);
   featured$: Observable<Game[]>;
   ongoing$: Observable<Game[]>;
@@ -35,6 +38,8 @@ export class LandingComponent {
 
   showSearchBar = false;
   searchText = "";
+
+  protected practiceAreaRoute?: UrlTree;
 
   constructor(
     private router: Router,
@@ -65,6 +70,11 @@ export class LandingComponent {
       switchMap(() => api.listGrouped({ filter: ['future', "competitive"], isOngoing: false, term: this.searchText })),
       tap(g => { if (g.length > 0) { this.showSearchBar = true; } })
     );
+  }
+
+  public async ngOnInit(): Promise<void> {
+    if (await this.practiceService.isEnabled())
+      this.practiceAreaRoute = this.routerService.getPracticeAreaUrl();
   }
 
   selected(game: Game | BoardGame): void {
